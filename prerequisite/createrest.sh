063017b95c41cc434830ef9e7546ac247220acde
Sanjay Tiwari <sanjay.180505@gmail.com>
2021-09-20 11:16:50 -0400

    Update Code

--- prerequisite/createrest.sh
+++ prerequisite/createrest.sh
@@ -2,3 +2,4 @@
 GITHUB_SECRET=$1
 kubectl create configmap kube-context -n workflows --from-file${HOME}/.kube/config 
 kubectl create secret generic github-access --from-literal=token=${GITHUB_SECRET}
+
